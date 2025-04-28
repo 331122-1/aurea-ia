dcb4ce548f000892900e9f7255cbd9f960fabcff
331122-1 <michagnur3@gmail.com>
2025-04-28 11:07:22 -0400

    aurea ia

---
+++ deepseek_bash_20250428_034a62.sh
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "Primera versión de la landing page de Aurea IA"
+git branch -M main
+git remote add origin https://github.com/tu-usuario/nombre-repositorio.git
+git push -u origin main
